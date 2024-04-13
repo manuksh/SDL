e23c9a84a68d6979d2e2f132c6b4544ff71718da
Manouk Shemsyan <manuk.shemsyan@gmail.com>
2024-04-13 21:09:32 +0400

    modified:   python/descripto_dbconnection.py

--- python/descripto_dbconnection.py
+++ python/descripto_dbconnection.py
@@ -1,4 +1,4 @@
-A db connection descriptor" typically refers to a set of information needed to connect to a database. 
+"""A db connection descriptor" typically refers to a set of information needed to connect to a database. 
 This information usually includes:
 
     Database URL: This specifies the location of the database server, including the protocol 
@@ -14,3 +14,57 @@ Other programming languages and frameworks have their own equivalent mechanisms.
 
     Additional Connection Parameters: Optional parameters that might be required for connection tuning or additional security settings. 
 These could include options like SSL/TLS settings, connection pool settings, or specific SQL dialect configurations.
+"""
+
+import json
+
+class Descriptor_DBConnection:
+    def __init__(self, name, description, database_type, host, port, username, password):
+        self.type = 'Descriptor_DBConnection'
+        self.name = name
+        self.description = description
+        self.database_type = database_type
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+
+    def get_descriptor_dict(self):
+        return {
+            "Type": self.type,
+            "Name": self.name,
+            "Description": self.description,
+            "Database_Type": self.database_type,
+            "Host": self.host,
+            "Port": self.port,
+            "Username": self.username,
+            "Password": self.password
+        }
+
+    def get_descriptor(self):
+        return json.dumps(self.get_descriptor_dict(), indent=4)
+
+def test():
+    # Create an instance of Descriptor_DBConnection
+    db_descriptor = Descriptor_DBConnection(
+        name="MyDatabase",
+        description="Represents a database connection.",
+        database_type="MySQL",
+        host="localhost",
+        port=3306,
+        username="root",
+        password="password123"
+    )
+
+    # Print the descriptor
+    print(db_descriptor.get_descriptor())
+
+
+
+# Example usage:
+if __name__ == "__main__":
+    test()
+
+
+
+    
